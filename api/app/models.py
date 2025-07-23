1b7dfc77d549410fadc660dcb5a8943fa67dae2c
Estefania Mazo Uribe <94013686+emazou@users.noreply.github.com>
2025-07-23 13:32:35 -0500

    Delete api/app/models.py

--- api/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class Book(SQLModel, table=True):
-    id:       int    = Field(default=None, primary_key=True)
-    name:     str
-    price:    float
-    pages:    int
-    author:   str
-    is_sold:  bool   = False
